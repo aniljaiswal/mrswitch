c05e3a286b789a96e255645a492f468aad00e0ce
Anil Jaiswal <er.anil.jaiswal@gmail.com>
2015-07-10 00:29:36 +0530

    removed not yet required routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,35 +19,9 @@ Route::get('/', function () {
     return view('welcome')->with('title', '24x7 Eletricians at your service, Delhi & NCR | Mr. Switch');
 });
 
-// /**
-//  * Authentication routes...
-//  */
-
-// Route::get('auth/login', 'Auth\AuthController@getLogin');
-// Route::post('auth/login', 'Auth\AuthController@postLogin');
-// Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-
-// // Registration routes...
-// Route::get('auth/register', 'Auth\AuthController@getRegister');
-// Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-
-// // Password reset link request routes...
-// Route::get('password/email', 'Auth\PasswordController@getEmail');
-// Route::post('password/email', 'Auth\PasswordController@postEmail');
-
-
-// // Password reset routes...
-// Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-// Route::post('password/reset', 'Auth\PasswordController@postReset');
-
 /**
  * Static Pages
  */
-
-//About Page
-Route::get('about', 'PagesController@about');
 
 //Contact Page
 Route::get('contact', 'PagesController@getContact');
@@ -61,9 +35,6 @@ Route::get('terms', 'PagesController@terms');
 
 //Refunds Page
 Route::get('refunds', 'PagesController@refunds');
-
-//Pricing Page
-Route::get('pricing', 'PagesController@pricing');
 
 //Newsletter Subscription POST
 Route::post('subscribe', ['as' => 'subscribe', 'uses' => 'PagesController@postSubscribe']);
